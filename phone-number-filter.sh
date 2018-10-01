9467176a287b68c1887fdc76243fb8fb54b1fde1
shirleytruffierblanc <40561239+shirleytruffierblanc@users.noreply.github.com>
2018-10-01 09:38:39 -0500

    Update and rename phonenumberfilter.sh to phone-number-filter.sh

---
+++ phone-number-filter.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+grep  "^+512 [0-9][0-9][0-9] [0-9][0-9][0-9][0-9]$" $1 > result-phonenumber-filter.txt
+
+
--- phonenumberfilter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-grep  "^+512 [0-9][0-9][0-9] [0-9][0-9][0-9][0-9]$" $1 > resultphonenumberfilter.txt
-
-
