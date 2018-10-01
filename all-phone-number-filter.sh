0e7c15a462cb56813fcce3a58dc9a94ec4799a7b
shirleytruffierblanc <40561239+shirleytruffierblanc@users.noreply.github.com>
2018-10-01 16:39:57 -0500

    Update all-phone-number-filter.sh

--- all-phone-number-filter.sh
+++ all-phone-number-filter.sh
@@ -1,5 +1,5 @@
 
-grep -E '^[ ]{0,9}\([0-9]{3}\) [0-9]{3}-[0-9]{4}[ ]{0,9}$' data.txt | sed 's/^[ \t]*//' > result-phonenumber-filter.txt
-count=$(wc -l result-phonenumber-filter.txt)
+grep -E '^[ ]{0,9}\([0-9]{3}\) [0-9]{3}-[0-9]{4}[ ]{0,9}$' data.txt | sed 's/^[ \t]*//' > all-result-phonenumber-filter.txt
+count=$(wc -l all-result-phonenumber-filter.txt)
 
-echo "The number of line is :$count" >> result-phonenumber-filter.txt
+echo "The number of line is :$count" >> all-result-phonenumber-filter.txt
